1d432308468cfcce0e02463d104663afd7878523
Eduardo Moreno <eledmolo@gmail.com>
2017-11-20 21:40:09 -0700

    corrección de errores menores

--- TuaregWeb/Tablero/assets/js/link.js
+++ TuaregWeb/Tablero/assets/js/link.js
@@ -6,7 +6,9 @@ var htm=document.getElementById('docum');
 var w = $(document).height();
 note.onclick=function(event){
     if(seMuestra==false){
+        
           htm.style.overflowY='scroll';
+        htm.style.maxWidth='100%';
           texto.style.display='block';
         
     seMuestra=true;
@@ -17,14 +19,21 @@ note.onclick=function(event){
         
         texto.style.display='none';
        seMuestra=false;
+         window.scrollTo(0,-900);
+        if(w<=1100){
+            htm.style.overflowY='scroll';
+            
+        }else
+            {
         htm.style.overflowY='hidden';
-        
+            }
       }    
     }
 }
 htm.onresize=function(event){
 if(w<=1100){
         htm.style.overflowY='scroll';
+    htm.style.width=1100;
         }
 }
 
