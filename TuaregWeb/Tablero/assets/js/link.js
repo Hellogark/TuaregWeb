c9fc05f46664afab750fcc277d2f2c706361b06d
Eduardo Moreno <eledmolo@gmail.com>
2017-11-20 21:19:47 -0700

    Se añadieron funciones de diseño al tablero

--- TuaregWeb/Tablero/assets/js/link.js
+++ TuaregWeb/Tablero/assets/js/link.js
@@ -2,17 +2,29 @@
 var texto = document.getElementById('txtArea');
 var note= document.getElementById('notas');
 var seMuestra=false;
-
+var htm=document.getElementById('docum'); 
+var w = $(document).height();
 note.onclick=function(event){
     if(seMuestra==false){
+          htm.style.overflowY='scroll';
           texto.style.display='block';
+        
     seMuestra=true;
         
+        
     }else{if(seMuestra=true){
+        window.scrollTo(900,0);
+        
         texto.style.display='none';
        seMuestra=false;
+        htm.style.overflowY='hidden';
         
       }    
     }
+}
+htm.onresize=function(event){
+if(w<=1100){
+        htm.style.overflowY='scroll';
+        }
 }
 
