f61ed368447d69638219434ea780952170202cee
Eduardo Moreno <eledmolo@gmail.com>
2017-11-20 20:13:59 -0700

    Revert "se añadieron atributos de posicionamiento a la página"
    This reverts commit 9c14382fd73d878c47018e6c1ef3f45e74a10e20.

--- TuaregWeb/Tablero/assets/js/link.js
+++ TuaregWeb/Tablero/assets/js/link.js
@@ -2,21 +2,15 @@
 var texto = document.getElementById('txtArea');
 var note= document.getElementById('notas');
 var seMuestra=false;
-var htm=document.getElementById('docum'); 
 
 note.onclick=function(event){
     if(seMuestra==false){
-          htm.style.overflowY='scroll';
           texto.style.display='block';
-        
     seMuestra=true;
         
     }else{if(seMuestra=true){
-        window.scrollTo(900,0);
-        
         texto.style.display='none';
        seMuestra=false;
-        htm.style.overflowY='hidden';
         
       }    
     }
